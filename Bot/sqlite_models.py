f52db04509238cedde2e6a9d251c88e8c4f751d1
Marcus Norrgard <marcus.norrgard@walkbase.com>
2019-10-15 19:51:51 +0300

    extend db model with telegram stuff

--- Bot/sqlite_models.py
+++ Bot/sqlite_models.py
@@ -1,5 +1,5 @@
 class User:
-        
+
     def __init__(self, discord_id, name, score=0, status=0):
         self.discord_id = discord_id
         self.name = name
@@ -9,10 +9,30 @@ class User:
     def __repr__(self):
         return self.name
 
+
 class Bttn():
 
     def __init__(self, discord_id, party_name, timestamp):
         self.discord_id = discord_id
         self.party_name = party_name
         self.timestamp = timestamp
-        
+
+
+class TelegramUser:
+
+    def __init__(self, telegram_id, name, score=0, status=0):
+        self.telegram_id = telegram_id
+        self.name = name
+        self.score = score
+        self.status = status
+
+    def __repr__(self):
+        return self.name
+
+
+class TelegramBttn():
+
+    def __init__(self, telegram_id, party_name, timestamp):
+        self.telegram_id = telegram_id
+        self.party_name = party_name
+        self.timestamp = timestamp
